5dca6c4e577bdaad2e80246fc045c2a9220967dd
PARK SUNG DONG <pasudo123@naver.com>
2019-07-05 17:22:42 +0900

    [feat] add common frontend vue file

---
+++ frontend/src/components/PaHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="header">
+        <h2>PASUDO123 Real World</h2>
+        <div class="nav">
+            네비게이션 넣기
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "PaHeader",
+    }
+</script>
+
+<style scoped>
+    header.header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 15px;
+        font-family: Consolas;
+    }
+</style>
--- frontend/src/components/PaMain.vue
+++ frontend/src/components/PaMain.vue
@@ -1,21 +1,31 @@
 <template>
     <div id="PaMain">
-        <todo-input-wrapper></todo-input-wrapper>
-        <todo-element-wrapper></todo-element-wrapper>
+        <PaHeader></PaHeader>
+
+        <!-- 라우터가 컴포넌트를 그려주기 위한 부분 -->
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
 
-    import TodoInputWrapper from './TodoInputWrapper';
-    import TodoElementWrapper from './TodoElementWrapper';
+    import PaHeader from './PaHeader'
+    import TodoMain from './todo/TodoMain'
 
     export default {
         name: "PaMain",
-        components: {TodoElementWrapper, TodoInputWrapper},
+        components: {PaHeader, TodoMain}
     }
 </script>
 
 <style scoped>
-
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        line-height: 1.4;
+    }
 </style>
