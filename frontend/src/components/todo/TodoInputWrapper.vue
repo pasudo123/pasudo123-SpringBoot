9bc69af3fc21c6748ef567c10f9cb7c35e5033b9
PARK SUNG DONG <pasudo123@naver.com>
2019-07-05 17:20:11 +0900

    [feat] add Todo frontend vue.js

--- frontend/src/components/todo/TodoInputWrapper.vue
+++ frontend/src/components/todo/TodoInputWrapper.vue
@@ -1,15 +1,16 @@
-
 <template>
-    <div>
+    <div class="todoForm">
         <v-form>
             <v-container>
-                <v-layout row wrap>
+                <v-layout align="center">
                     <v-flex>
                         <v-text-field
+                                class="todoTextField"
                                 v-model='todoText'
                                 label="할 일 작성하기"
                                 solo-inverted
                         ></v-text-field>
+                        <a class="createTodoBtn" href="">등록하기</a>
                     </v-flex>
                 </v-layout>
             </v-container>
@@ -20,18 +21,26 @@
 <script>
     export default {
         name: "TodoInputWrapper",
-        data: function() {
-            return{
+        data: function () {
+            return {
                 todoText: '',
             }
         },
-        methods: {
-
-        },
+        methods: {},
 
     }
 </script>
 
 <style scoped>
-
+    a.createTodoBtn{
+        padding:14px 18px;
+        background: #333;
+        color: #fff;
+        text-align: center;
+        font-family: Consolas;
+        font-size:15px;
+        text-decoration: none;
+        /*position: absolute;*/
+        /*right:0;*/
+    }
 </style>
