9cd74990ec75e2d75e8796096365d1974cb61066
PARK SUNG DONG <pasudo123@naver.com>
2019-07-06 21:33:51 +0900

    [feat] add router ( / , /about )

--- frontend/src/components/PaHeader.vue
+++ frontend/src/components/PaHeader.vue
@@ -1,8 +1,10 @@
 <template>
     <header class="header">
         <h2>PASUDO123 Real World</h2>
+
         <div class="nav">
-            네비게이션 넣기
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
         </div>
     </header>
 </template>
@@ -19,6 +21,17 @@
         color: #fff;
         text-align: center;
         padding: 15px;
+        font-family: Consolas;
+    }
+
+    div.nav{
+        margin-top:7px;
+    }
+
+    header.header a{
+        color: #fff;
+        padding: 0px 3px;
+        text-decoration: none;
         font-family: Consolas;
     }
 </style>
