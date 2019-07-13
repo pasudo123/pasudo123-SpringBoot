c6398e2bdbce7a61a697e1c21e21618518851e78
PARK SUNG DONG <pasudo123@naver.com>
2019-07-13 23:24:40 +0900

    [feat] update vue basical config set

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -1,15 +1,53 @@
 <template>
-    <div>
+    <div class="HomeWrapper">
         <h1>Home</h1>
+        <div class="list"
+             v-for="link in AllLinks"
+             v-bind:key="link.id">
+            <a @click="convertView(link)">{{link.name}}</a>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        data() {
+            return {
+                AllLinks: [],
+            }
+        },
+        methods: {
+            setLink() {
+                let array = ["todo", "album"];
+
+                for (let i = 0; i < array.length; i++) {
+                    this.AllLinks.push({
+                        id: i,
+                        link: "/" + array[i],
+                        name: array[i]
+                    });
+                }
+            },
+            convertView(link){
+                this.$router.push({path: link.link})
+            }
+        },
+        created() {
+            this.setLink();
+        }
     }
 </script>
 
 <style scoped>
+    div.HomeWrapper {
+        margin-top: 20px;
+        text-align: center;
+    }
+
+    div.list{
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
 
 </style>
